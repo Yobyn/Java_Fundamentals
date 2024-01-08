3afb2b84e95eeec2296a077085504028f7e86dbf
Yobyn Roetz <yobyn@Yobyns-MBP.home>
2024-01-08 11:50:00 +0100

    Inner class

--- Outer.java
+++ Outer.java
@@ -2,8 +2,23 @@ public class Outer {
 
   private String greeting = "Hi";
 
-  protected class Inner {
+  final protected class Inner {
     public int repeat = 3;
+    public void go() {
+      for (int i =0; i < repeat; i ++){
+        System.out.println(greeting);
+      }
+    }
+  }
+
+  public void callInner(){
+    Inner inner = new Inner();
+    inner.go();
+  }
+
+  public static void main(String[] args){
+    Outer outer = new Outer();
+    outer.callInner();
   }
 
 }
