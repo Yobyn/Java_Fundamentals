eaf4183747d4b7ce94fd57b1eba3f572c385bdd1
Yobyn Roetz <yobyn@Yobyns-MBP.home>
2024-01-08 11:58:54 +0100

    calling 'new' as method on Outer class

--- Outer.java
+++ Outer.java
@@ -6,19 +6,15 @@ public class Outer {
     public int repeat = 3;
     public void go() {
       for (int i =0; i < repeat; i ++){
-        System.out.println(greeting);
+        System.out.println(greeting); // Inner class can access methods of the outer class
       }
     }
   }
 
-  public void callInner(){
-    Inner inner = new Inner();
-    inner.go();
-  }
-
   public static void main(String[] args){
     Outer outer = new Outer();
-    outer.callInner();
+    Inner inner = outer.new Inner();
+    inner.go();
   }
 
 }
